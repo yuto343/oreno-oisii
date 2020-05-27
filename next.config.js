6c935eebe2703e667d5fc6a7b80338f403e1bd77
Yuto-Nakano <namawasabi1229@gmail.com>
2020-05-27 21:08:14 +0900

    nextconfigupdate

--- next.config.js
+++ next.config.js
@@ -6,8 +6,17 @@ module.exports = withOptimizedImages({
   imageTrace: {
     color: "#fff",
   },
+  webp: {
+    autoFilter: true,
+  },
   webpack(config) {
     config.resolve.alias.images = path.join(__dirname, "images");
+
+    //webpacが.mdファイルを読みこんで、エラーを吐くのに対する対策
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
     return config;
   },
 });
